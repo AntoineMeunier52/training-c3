1a8e00024af869567811bf8a2ee08303252ca73d
AntoineMeunier52 <meunier.antoine52@gmail.com>
2021-03-22 20:03:44 +0100

    save split

---
+++ ex02/ft_split.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+char *ft_strncpy(char *dest, char *src, unsigned int n)
+{
+    unsigned int i;
+
+    i = 0;
+    while (src[i] && i < n)
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i] = '\0';
+    return (dest);
+}
+
+char **ft_split(char *str, char *charset)
+{
+}
